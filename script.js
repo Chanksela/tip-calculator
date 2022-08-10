406ea6e03ef4a51abb7171e69ded7dfa09154e0c
chanksela <kakhachankseliani@gmail.com>
2022-08-10 15:52:32 +0400

    refactoring code. trying to add errorHandling

--- script.js
+++ script.js
@@ -1,37 +1,66 @@
-const resultDisplayElement = document.getElementById("display-input");
-const tipEachPersonElement = document.getElementById("display-tip");
-const percantageInputElement = document.getElementById("input-percentage-btn");
+// display elements
+const displayBillElement = document.getElementById("display-input");
+const displayPercantageElement = document.getElementById("display-percantage");
+const displayPeopleElement = document.getElementById("display-people");
+const displayTipElement = document.getElementById("display-tip");
+const displayBillError = document.getElementById("bill-error");
+// input elements
 const userInputElement = document.getElementById("bill");
+const percantageInputElement = document.getElementById("percentage-input");
 const percentageButtonElement =
   document.getElementsByClassName("percentage-btn");
-console.log(percentageButtonElement);
-let userInputValue;
-let buttonValue;
-
-// save and display bill input value
-function billAmount() {
-  userInputValue = userInputElement.value;
-  resultDisplayElement.innerHTML = userInputValue;
+const numberOfPeopleInputElement = document.getElementById(
+  "number-of-people-input"
+);
+// variables
+let billInputValue;
+let percentageValue;
+let numOfPeopleValue;
+// get bill amount
+function getBillAmount() {
+  billInputValue = userInputElement.value;
+  console.log(billInputValue);
+  if (billInputValue > 0) {
+    displayBillElement.innerHTML = billInputValue;
+  } else {
+    displayBillError.innerHTML = "input must be greater than 0";
+  }
 }
 
+// get percentage
+function getPercantage() {
+  percentageValue = percentageButtonElement.value;
+  displayPercantageElement.innerHTML = percentageValue;
+}
+// get ppl amount
+function getNumberOfPeople() {
+  numOfPeopleValue = numberOfPeopleInputElement.value;
+  displayPeopleElement.innerHTML = numOfPeopleValue;
+}
 // calculate on clicking percantage buttons
-function handleBtnClick(percantageValue, percantageButton, index, e) {
-  percantageValue.innerText =
-    (userInputValue * percantageButton[index].value) / 100;
+function handlePercantageBtnClick(percantageValue, percantage, index) {
+  console.log("Clicked Button %: ", percantage[index].value);
+  percantageValue.innerText = percantage[index].value;
 }
-function test(percantageValue, percantageButton, index, e) {
-  percantageValue.innerText = (userInputValue * percantageButton.value) / 100;
+function handlePercantageInputClick(percantageValue, percantage) {
+  percantageValue.innerText = percantage.value;
 }
-console.log(userInputValue);
 for (let i = 0; i < percentageButtonElement.length; i++) {
   function calculateTip(e) {
-    if (e.target.id === "input-percentage-btn") {
-      test(tipEachPersonElement, percantageInputElement, i);
+    if (e.target.id === "percentage-input") {
+      handlePercantageInputClick(displayTipElement, percantageInputElement, i);
     } else {
-      handleBtnClick(tipEachPersonElement, percentageButtonElement, i);
+      handlePercantageBtnClick(displayTipElement, percentageButtonElement, i);
     }
   }
+
   percentageButtonElement[i].addEventListener("click", calculateTip);
   percantageInputElement.addEventListener("change", calculateTip);
 }
-userInputElement.addEventListener("change", billAmount);
+
+function handleButtonPercantage() {
+  percentageValue = percantage[index].value;
+  displayPercantageElement.innerHTML = percentageValue;
+}
+userInputElement.addEventListener("change", getBillAmount);
+numberOfPeopleInputElement.addEventListener("change", getNumberOfPeople);
