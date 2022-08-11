6622f5bd05543e5447fc730e99ed1ef0051fa139
chanksela <kakhachankseliani@gmail.com>
2022-08-11 13:37:52 +0400

    added eventListeners with input. code is much better now. need to work on it more

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let bill;
+let tip;
+let people;
+let tipTotalAmount;
+let tipForEachPerson;
 const errorMessage = "Must be greater than 0";
 // display elements
 const displayBillElement = document.getElementById("display-input");
@@ -16,3 +21,32 @@ const tipButtonElement = document.getElementsByClassName("tip-btn");
 const numberOfPeopleInputElement = document.getElementById(
   "number-of-people-input"
 );
+function getValue(event) {
+  let target = event.target.id;
+  console.log(target);
+  if (target === "bill") {
+    bill = parseFloat(event.target.value);
+    displayBillElement.innerHTML = bill;
+  } else if (target === "tip-input") {
+    tip = parseFloat(event.target.value);
+    displayTipElement.innerHTML = tip;
+  } else if (target === "number-of-people-input") {
+    people = parseFloat(event.target.value);
+    displayPeopleElement.innerHTML = people;
+  }
+}
+function calculate(e) {
+  getValue(e);
+  if (bill > 0 && tip > 0) {
+    tipTotalAmount = bill + (bill * tip) / 100;
+    displayFullTip.innerHTML = tipTotalAmount;
+  }
+  if (bill > 0 && tip > 0 && people > 0) {
+    tipForEachPerson = tipTotalAmount / people;
+    displayTipForEach.innerHTML = tipForEachPerson;
+  }
+  displayFullTip.innerHTML = parseFloat(tipAmount);
+}
+billInputElement.addEventListener("input", (e) => calculate(e));
+tipInputElement.addEventListener("input", (e) => calculate(e));
+numberOfPeopleInputElement.addEventListener("input", (e) => calculate(e));
